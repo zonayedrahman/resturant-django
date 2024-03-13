3a90d8a56ada455799b2f3f6404a7a674b8ced26
zonayedrahman <zr578@nyu.edu>
2024-03-13 19:03:55 -0400

    Changes to initial

--- littlelemon/asgi.py
+++ littlelemon/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for returant_django project.
+ASGI config for littlelemon project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "returant_django.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "littlelemon.settings")
 
 application = get_asgi_application()
